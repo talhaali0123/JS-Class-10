8857324aa2802d9c855672993a9e2cc615178e72
talhaali0123 <50287643+talhaali0123@users.noreply.github.com>
2024-10-01 20:48:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+// var input = prompt("Enter any word");
+
+// var reserveWord = "";
+
+// for ( var i = input.length -1 ; i >= 0 ; i--) {
+//     reserveWord += input[i];
+// }
+
+// if (input === reserveWord) {
+//     console.log("It's a Palindrome word")
+// } else {
+//     console.log("It's not a Palindrome word")
+// }
+
+
+// var date = new Date();
+
+// console.log(date);
+
+// var daysArr = [
+//     'Sunday',
+//     'Monday',
+//     'Tuesday',
+//     'Wednesday',
+//     'Thursday',
+//     'Friday',
+//     'Saturday'
+// ];
+
+// console.log(daysArr[date.getDay()]);
+
+// console.log(date.toLocaleTimeString());
+
+// Age calculator Program
+
+// var birthYear = +prompt("Enter your Birth Year");
+
+// var date = new Date();
+
+// console.log(date);
+
+// var currentYear =  date.getFullYear();
+
+// var age = currentYear - birthYear;
+
+// console.log(age);
+
+// var firstRamadan = new Date("March 1, 2025").getTime();
+
+// var currentDate = new Date().getTime();
+
+// var diff = firstRamadan - currentDate;
+
+// var month = diff / (1000 * 60 * 60 *24 * 30);
+
+// console.log(Math.round(month));
+
+
+console.log(NaN);
+
+
+
